f9d7fdddc12a63cd243320ca56ccd7394f5466e3
Christian34190 <85951756+Christian34190@users.noreply.github.com>
2021-06-23 19:12:27 +0200

    Testplugin tcpdf

---
+++ start.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('ABSPATH') or die("Thanks for visting");
+  /**
+  * Plugin Name:  FiMint test
+  * Plugin URI: https://fimint-online.de
+  * Description: Ein Testplugin zur Behebung von TCPDF - Problemen
+  * Version: 2.0
+  * Author: FiMint
+  * Author URI: https://fimint-online.de
+  * License: GPL2
+  * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+  * Text Domain:  Registrierfomularv2
+  */
+
+
+  add_shortcode('pdf2','pdf2');
+  require 'test.php';
+
+?>
